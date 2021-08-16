3460927b121d2c0b3fce8c5657232ab438f6e580
wwalter <william.walter@mongodb.com>
2021-08-16 10:55:19 -0400

    first pass at cpu stress

--- main.go
+++ main.go
@@ -18,6 +18,8 @@ var successCount int
 type config struct {
 	resolverCount *int
 	duration *int
+	memory *bool
+	cpu *bool
 }
 
 var conf config
@@ -31,17 +33,23 @@ func init(){
 
 	conf.resolverCount =  flag.Int("r", defaultResolverCount, "The number of resolver routines to run, default: 2")
 	conf.duration =  flag.Int("d", defaultDuration, "The duration of the test, default: 60")
+	conf.memory =  flag.Bool("m", false, "run memory tests, default: false")
+	conf.cpu =  flag.Bool("c", false, "run cpu intensive, default: false")
 	rand.Seed(time.Now().UnixNano())
 }
 
 func main() {
 	flag.Parse()
 
-	aboutAGig := (1024 * 1024 * 1024) /4
-	meg := make([]int32, aboutAGig)
 
-	for i := range meg {
-		meg[i] = int32(1)
+	var meg []int32
+	if *conf.memory {
+		aboutAGig := (1024 * 1024 * 1024) /4
+		meg = make([]int32, aboutAGig)
+
+		for i := range meg {
+			meg[i] = int32(1)
+		}
 	}
 
 	printMemUsage()
@@ -49,6 +57,12 @@ func main() {
 	tock := time.NewTicker(time.Duration(10) * time.Second)
 	defer tock.Stop()
 	done := make(chan bool)
+
+	if *conf.cpu {
+		rand.Seed(time.Now().UnixNano())
+		go cpuStress(done)
+		go cpuStress(done)
+	}
 	go func(m []int32) {
 		for {
 			select {
@@ -56,7 +70,9 @@ func main() {
 				break
 			case t := <-tock.C:
 				pct := errCount/(errCount + successCount)
-				m[rand.Int31n(35000)] = rand.Int31n(35000)
+				if *conf.memory {
+					m[rand.Int31n(35000)] = rand.Int31n(35000)
+				}
 				fmt.Printf("%s err: %d\t suc: %d\t errPct: %d\n%s ", t, errCount, successCount, pct, t)
 				printMemUsage()
 //				debug.FreeOSMemory()
@@ -110,4 +126,19 @@ func printMemUsage() {
 
 func bToMb(b uint64) uint64 {
     return b / 1024 / 1024
+}
+
+func cpuStress(d chan bool) {
+	for {
+		select {
+		case <-d:
+			break
+		default:
+			bigA := ^uint64(0) - rand.Uint64()
+			bigB := ^uint64(0) - rand.Uint64()
+			if bigB == 0 { bigB++ }
+			_u := bigA / bigB
+			_u++
+		}
+	}
 }
